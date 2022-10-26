a2f25d148a13e45bb5159b7c0f1cc8ff90d7926f
WeslleyRoberto <83045145+WeslleyRoberto@users.noreply.github.com>
2022-10-26 13:01:14 -0300

    Initial commit

---
+++ inicial.py
@@ -0,0 +1,16 @@
+a = int(input("informe um número maior que 10: "))
+
+while a <= 10:
+    print("Número menor do que o esperado!!!")
+    a = int(input("Infome um número válido: "))
+    continue
+
+b = int(input("Agora informe um número multiplicador menor que 10: "))
+
+while b >= 10:
+    print("Número informado é maior que 10!!!")
+    b = int(input("Informe um número válido"))
+    continue
+
+print(f"A multiplicação entre os números informados é: {a * b}")
+
